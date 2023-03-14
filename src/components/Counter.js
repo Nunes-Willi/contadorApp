ae5c8e8eb4f644dc849e8f7d34533dd4fe3ed78c
Nunes-Will <nuneswillroberto@gmail.com>
2023-03-14 16:06:12 -0300

    Contador

---
+++ src/components/Counter.js
@@ -0,0 +1,35 @@
+import { Component } from "react";
+import { View, StyleSheet, Text, Button } from "react-native";
+
+export default class Counter extends Component {
+  state = {
+    count: 0,
+  };
+//   if() {
+//     count < 0
+//   }
+
+  incrementar() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  decrementar() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <View style={styles.contador}>
+        <Text>Contador: {this.state.count} </Text>
+        <Button onPress={() => this.incrementar()} title="+" />
+        <Button onPress={() => this.decrementar()} title="-" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  contador: {
+    flexDirection: "row",
+    alignItens: "center",
+  },
+});
